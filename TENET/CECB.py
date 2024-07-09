4a7327f13c162354084bb77934216bea78dca5bf
Yujian-Lee <161132007+Yujian-Lee@users.noreply.github.com>
2024-07-09 22:06:00 +0800

    Update CECB.py

--- TENET/CECB.py
+++ TENET/CECB.py
@@ -1,19 +1,19 @@
 import torch
 import torch.nn as nn
 import random
-
 class CECB(torch.nn.Module):
     def __init__(self, in_ch):
         super(CECB, self).__init__()
-        self.GAP = nn.AdaptiveAvgPool1d(512)
+        self.GAP = nn.AdaptiveAvgPool2d(1)
         self.FC1 = nn.Linear(in_ch, 32)
         self.ReLu = nn.ReLU()
         self.FC2 = nn.Linear(32, in_ch)
         self.Sigmoid = nn.Sigmoid()
-        self.BN = nn.BatchNorm1d(in_ch)
-        self.GAP2 = nn.AdaptiveAvgPool1d(512)
+        self.BN = nn.BatchNorm2d(in_ch)
+        self.GAP2 = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, Finput):
+        Finput = Finput.view([Finput.shape[0], Finput.shape[1], 1, 1])
         Fv = self.BN(Finput)
         Mv = Finput - Fv
         Mv_1 = self.GAP(Mv)
@@ -22,18 +22,22 @@ class CECB(torch.nn.Module):
         Mv_3 = self.ReLu(Mv_2)
         Mv_4 = self.FC2(Mv_3)
         Mv_5 = self.Sigmoid(Mv_4)
+        Mv_5 = Mv_5.view([Finput.shape[0], Finput.shape[1], 1, 1])
         Mv_x = torch.mul(Mv, Mv_5)
 
         cat = Fv + Mv_x
 
         catBN = self.BN(cat)
         catBN = self.GAP2(catBN)
+
+        catBN = catBN.view(catBN.shape[0], catBN.shape[1])
+
         return catBN
 
 if __name__ == "__main__":
     dummy_x_1 = torch.rand([1597,64])
     dummy_x_2 = torch.rand([71865, 32])
-    model1 = CECB(64)
+    model1 = CECB(64) # block test for the cell level feature dimension
     out_1 = model1(dummy_x_1)
     # out_2 = model1(dummy_x_2)
 
